8f229c98fa1a7570dd050ae09e8f96ce6c7b9dda
Iqra Javed <100509784+iqra487@users.noreply.github.com>
2023-09-20 17:21:46 +0500

    Add files via upload
    dbfirst

---
+++ Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirstApproach.models;
+
+public partial class Movie
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public DateTime ReleasedDate { get; set; }
+
+    public string Genre { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string? Desciption { get; set; }
+}
---
+++ MovieCrudContextContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbFirstApproach.models;
+
+public partial class MovieCrudContextContext : DbContext
+{
+    public MovieCrudContextContext()
+    {
+    }
+
+    public MovieCrudContextContext(DbContextOptions<MovieCrudContextContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source = DESKTOP-G4MUUKE\\SQLEXPRESS; Initial Catalog= MovieCrudContext;integrated Security = True; trusted_connection=true; encrypt = false");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.ToTable("Movie");
+
+            entity.Property(e => e.Desciption)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("desciption");
+            entity.Property(e => e.Genre).HasColumnName("genre");
+            entity.Property(e => e.Price)
+                .HasColumnType("decimal(18, 2)")
+                .HasColumnName("price");
+            entity.Property(e => e.ReleasedDate).HasColumnName("releasedDate");
+            entity.Property(e => e.Title).HasColumnName("title");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
